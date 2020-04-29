064f9bc942b9fa55daa4ce00f4b65c2bcebaf749
Eduardo Braz Barbaresco <moreedu@gmail.com>
2020-04-29 18:53:04 -0300

    Start: Estrutura Inicial do Projeto

---
+++ App.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.Container}>
+        <Text style={styles.welcome}>Hello World!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  Container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 24,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
